13b4d960a6b207928e47b0b7a466342511458922
annapoliswu <38405329+annapoliswu@users.noreply.github.com>
2021-11-11 16:49:05 -0500

    ehhh

---
+++ Assets/Scripts/GridGen3D.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGen3D : MonoBehaviour
+{
+    public GameObject seedObj;
+    public int xDim, yDim, zDim;
+    public float seedDist = 1.0f;
+
+    // The grid is generated at startup
+    void Start()
+    {
+
+        // set up grid values
+        Vector3 _curPosition = new Vector3();
+        float _xHalf, _yHalf, _zHalf;
+        _xHalf = xDim * 0.5f;
+        _yHalf = yDim * 0.5f;
+        _zHalf = zDim * 0.5f;
+
+        GameObject newObj;
+        for (int i = 0; i < yDim; i++)
+        {
+            for (int j = 0; j < xDim; j++)
+            {
+                for (int k = 0; k < zDim; k++)
+                {
+                    _curPosition.x = transform.position.x + (j - _xHalf) * seedDist;
+                    _curPosition.y = transform.position.y + (i - _yHalf) * seedDist;
+                    _curPosition.z = transform.position.z + (k - _zHalf) * seedDist;
+
+                    Quaternion qrot = Quaternion.Euler(0f, 0f, 0f);
+                    newObj = GameObject.Instantiate(seedObj, _curPosition, Quaternion.identity) as GameObject;
+
+                }
+
+            }
+        }
+        newObj = GameObject.Instantiate(seedObj, _curPosition, Quaternion.identity) as GameObject;
+    }
+}
+
---
+++ Assets/Scripts/SeedGen3.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedGen3 : MonoBehaviour
+{
+    public GameObject[] parts;
+    public float[] rotations;
+    public Material[] materials;
+    public float scaleMin = 1.0f, scaleMax = 1.1f;
+    private Renderer rend;
+
+    // Start is used to set determine the element
+    void Start()
+    {
+        // first choose a model from the supplied array
+        GameObject newObj;
+        int objIndex = Random.Range(0, parts.Length);
+
+        // set the rotations from the supplied array
+        Quaternion qrot = Quaternion.Euler(rotations[Random.Range(0, rotations.Length)], rotations[Random.Range(0, rotations.Length)], 0f);
+
+        // instantiate the new game object
+        newObj = GameObject.Instantiate(parts[objIndex], transform.position, qrot) as GameObject;
+
+        // set the scale randomly between min and max
+        float newScale = Random.Range(scaleMin, scaleMax);
+        newObj.transform.localScale = new Vector3(newScale, newScale, newScale);
+
+        // Fetch the Renderer from the new GameObject
+        rend = newObj.GetComponent<Renderer>();
+
+        // choose the material from the array provided
+        int matIndex = Random.Range(0, materials.Length);
+        rend.material = materials[matIndex];
+
+        // Set the texture scale and offset
+        rend.material.SetTextureOffset("_BaseMap", new Vector2(0.2f, 0.7f));
+        float scale = Random.Range(0.1f, 1.0f);
+        rend.material.SetTextureScale("_BaseMap", new Vector2(scale, scale));
+    }
+
+}
+
+
